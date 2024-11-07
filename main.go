78a4c6c0c1e959d428ca21875371bb7eebf81718
IPrizOn <ipygachev333@gmail.com>
2024-11-07 11:14:20 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", HomePage)
+
+	fmt.Println("Server is listening...")
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
