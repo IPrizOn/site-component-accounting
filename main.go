a01a6b7fc52140b587e580a802ffef492f15749e
IPrizOn <ipygachev333@gmail.com>
2024-11-14 01:56:26 +0200

    third commit

--- main.go
+++ main.go
@@ -1,40 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"text/template"
 )
 
-var templates = template.Must(template.ParseGlob("templates/*.html"))
-
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/first", firstHandler)
-	http.HandleFunc("/second", secondHandler)
-	http.HandleFunc("/third", thirdHandler)
+	http.HandleFunc("/", HomePage)
+
+	fmt.Println("Server is listening...")
 
 	http.ListenAndServe(":8080", nil)
 }
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.html")
-}
-
-func firstHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "first.html")
-}
-
-func secondHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "second.html")
-}
-
-func thirdHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "third.html")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	err := templates.ExecuteTemplate(w, tmpl, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
 }
